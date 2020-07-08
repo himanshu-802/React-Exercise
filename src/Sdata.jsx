71631d10b895b32a088c58328c8b522111c11bf0
Himanshu Rathore <36191619+himanshu-802@users.noreply.github.com>
2020-07-08 17:10:10 +0530

    USed of Map function

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -21,6 +21,34 @@ const Sdata=[
         title:"A Netflix Original Series",
         link:"https://www.netflix.com/in/title/80100172",
     },
+    {
+        
+        sname:"Dark",
+        imgscr:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        
+        sname:"Dark",
+        imgscr:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        
+        sname:"Dark",
+        imgscr:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        
+        sname:"Dark",
+        imgscr:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80100172",
+    }
     
 ]
 export default Sdata;
--- src/index.js
+++ src/index.js
@@ -3,31 +3,49 @@ import ReactDOM from 'react-dom';
 import Card from './Cards';
 import './index.css';
 import Sdata from './Sdata';
+
+// function ncard(val, ind, arr) {
+//     return (
+//         <Card
+//             imgsrc={val.imgscr}
+//             title={val.title}
+//             sname={val.sname}
+//             link={val.link}
+//         />
+//     );
+// }
+const ncard=(val,ind,arr)=>{
+    return(
+        <Card
+            imgsrc={val.imgscr}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+        />
+    );
+}
 ReactDOM.render(
     <>
-    <h1 className="heading_style">List of top 5 Netflix Series in 2020</h1>
-    <Card
-    imgsrc={Sdata[0].imgscr}
-    title={Sdata[0].title}
-    sname={Sdata[0].sname}
-    link={Sdata[0].link}
-    />
-    <Card
-    className="kuchbhi"
-    imgsrc={Sdata[1].imgscr}
-    title={Sdata[1].title}
-    sname={Sdata[1].sname}
-    link={Sdata[1].link}
-    />
-    <Card
-    imgsrc={Sdata[2].imgscr}
-    title={Sdata[2].title}
-    sname={Sdata[2].sname}
-    link={Sdata[2].link}
-    />
+        <h1 className="heading_style">List of top 5 Netflix Series in 2020</h1>
+        {Sdata.map(ncard)} 
+        {/* Can comment the above */}
+        { Sdata.map( (val, index) => {
+
+            console.log('this is index number ' + index);
+            return(                
+                <Card
+            imgsrc={val.imgscr}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+        />
+
+            );
+        })}
     </>,
     document.getElementById("root")
 )
+
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
